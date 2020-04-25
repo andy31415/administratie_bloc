399f328fbe21255887616e7c874e2b15c3bdf9e9
Andrei <andy314@gmail.com>
2020-04-25 12:11:44 -0400

    Start adding proprietars

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_25_150911) do
+ActiveRecord::Schema.define(version: 2020_04_25_160845) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -67,6 +67,18 @@ ActiveRecord::Schema.define(version: 2020_04_25_150911) do
     t.integer "tip"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "proprietari", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_proprietari_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_proprietari_on_reset_password_token", unique: true
   end
 
   create_table "scari", force: :cascade do |t|
---
+++ test/models/proprietari_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ProprietariTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
